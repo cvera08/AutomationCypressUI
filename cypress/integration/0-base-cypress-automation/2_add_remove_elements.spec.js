b3983ba490f811fa1c9febcc7fa031803d1c866f
cvera <cvera08@gmail.com>
2022-04-25 18:45:26 -0300

    reformat code *.spec.js

--- cypress/integration/0-base-cypress-automation/2_add_remove_elements.spec.js
+++ cypress/integration/0-base-cypress-automation/2_add_remove_elements.spec.js
@@ -1,25 +1,25 @@
 /// <reference types="cypress" />
 
 describe('Button Click', () => {
-  beforeEach(() => {
-    cy.visit('https://the-internet.herokuapp.com/add_remove_elements/')
-  })
+
   const ADD_BUTTON = 'button[onclick="addElement()"]';
   const DELETE_BUTTON = '#elements button';
 
+  beforeEach(() => {
+      cy.visit('https://the-internet.herokuapp.com/add_remove_elements/')
+  })
 
- it('displays "Add Element" button by default', () => {
-    cy.get(ADD_BUTTON).should('have.length', 1)
+  it('displays "Add Element" button by default', () => {
+      cy.get(ADD_BUTTON).should('have.length', 1)
   })
 
   it('user is able to click "Add Element"', () => {
-    cy.get(ADD_BUTTON).click()
+      cy.get(ADD_BUTTON).click()
   })
 
   it('user is able to click "Delete" after "Add Element"', () => {
-    cy.get(ADD_BUTTON).click()
-    cy.get(DELETE_BUTTON).click()
-  })
+      cy.get(ADD_BUTTON).click()
+      cy.get(DELETE_BUTTON).click()
   })
 
-  
+})
