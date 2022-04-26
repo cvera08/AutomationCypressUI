08d18decf2f3932e9320052c109db0daa7741bf2
cvera <cvera08@gmail.com>
2022-04-26 11:17:54 -0300

    new checkbox tests

---
+++ cypress/integration/0-base-cypress-automation/3_click_checkboxes.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Checkbox Click', () => {
+
+    const CHECKBOXES = '#checkboxes>input';
+
+    beforeEach(() => {
+        cy.visit('https://the-internet.herokuapp.com/checkboxes')
+    })
+
+    it('check checkbox 1', () => {
+        cy.get(CHECKBOXES).first().check() //get the first checkbox
+    })
+
+    it('uncheck checkbox 2', () => {
+        cy.get(CHECKBOXES).eq(1).uncheck() //get the second checkbox
+    })
+
+})
